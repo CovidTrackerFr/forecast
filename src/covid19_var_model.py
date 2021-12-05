ec9cdaf4203a6922682796dcd9329b9df9357814
rozierguillaume <guillaume.rozier73@gmail.com>
2021-12-05 18:22:14 +0100

    fix

--- src/covid19_var_model.py
+++ src/covid19_var_model.py
@@ -65,3 +65,5 @@ def forecast_and_plot():
 
         # Plot
         plot_and_export(df_dlog_all, yhat_mean, yhat_conf_int_0, yhat_conf_int_1, yhat_conf_int_2, yhat_conf_int_3, yhat_conf_int_75_0, yhat_conf_int_75_1, yhat_conf_int_75_2, yhat_conf_int_75_3, name=name_plot, )
+
+forecast_and_plot()
